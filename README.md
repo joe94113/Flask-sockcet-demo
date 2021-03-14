3580f38df9aa9cc8a858411bc771df53258b5646
joe <joe94113@gmail.com>
2021-03-14 14:06:50 +0800

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-"# Flask-sockct-demo" 
+# Flask-sockct-demo
+
+### Pycharm Parameters： gunicorn -k flask_sockets.worker test:app
+
+### 在URL輸入：http://localhost:8000/echo_test
---
+++ test.py
@@ -0,0 +1,34 @@
+from flask import Flask, render_template
+from flask_sockets import Sockets
+
+app = Flask(__name__)
+app.debug = True
+
+sockets = Sockets(app)
+
+
+@sockets.route('/echo')
+def echo_socket(ws):
+    while True:
+        message = ws.receive()
+        if (message == "socket open"):
+            ws.send("歡迎使用客服機器人")
+        else:
+            ws.send(message)
+
+
+@app.route('/')
+def hello():
+    return 'Hello World!'
+
+
+@app.route('/echo_test', methods=['GET'])
+def echo_test():
+    return render_template('index.html')
+
+
+if __name__ == '__main__':
+    from gevent import pywsgi
+    from geventwebsocket.handler import WebSocketHandler
+    server = pywsgi.WSGIServer(('', 8000), app, handler_class=WebSocketHandler)
+    server.serve_forever()
